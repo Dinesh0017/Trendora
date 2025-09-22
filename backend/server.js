baea3d3afa8a3699547f22c2f4fc7ec312d74e37
Dinesh Rathnasiri <dineshrathnasiri017@gmail.com>
2025-09-22 19:57:01 +0530

    backend created

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './config/mongodb.js';
+
+//App Config
+const app = express();
+const port = process.env.PORT || 4000;
+connectDB();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+//API endpoints
+app.get('/', (req, res) => {
+    res.send('API working') // on browser, it will show API working
+});
+
+//Listener
+app.listen(port, () => console.log(`Listening on localhost:${port}`)); //showing in terminal
